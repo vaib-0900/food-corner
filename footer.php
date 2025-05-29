0277d7b20d887c52ab55c58cc9a5046463284129
Samarthh28 <samarthyadav2807@gmail.com>
2025-05-29 14:49:49 +0530

    footer added

---
+++ footer.php
@@ -0,0 +1,8 @@
+
+<footer class="bg-dark text-white text-center py-4 mt-5">
+    <p>&copy; 2025 FoodCorner. All rights reserved.</p>
+</footer>
+
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js"></script>
+</body>
+</html>
