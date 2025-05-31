42ad948dc1e6d95a2af554348b03544e05258596
Samarthh28 <samarthyadav2807@gmail.com>
2025-05-31 10:38:32 +0530

    footer changes

--- footer.php
+++ footer.php
@@ -1,8 +1,28 @@
+<style>
+    /* Footer */
+        .footer {
+            background-color: #343a40;
+            color: #fff;
+            padding: 30px 0;
+            text-align: center;
+        }
 
-<footer class="bg-dark text-white text-center py-4 mt-5">
+        .footer a {
+            color: #ffc107;
+            text-decoration: none;
+        }
+
+        .footer a:hover {
+            text-decoration: underline;
+        }
+</style>
+<!-- Footer -->
+<footer class="footer">
     <p>&copy; 2025 FoodCorner. All rights reserved.</p>
+    <p>Follow us on <a href="#">Instagram</a> | <a href="#">Facebook</a> | <a href="#">Twitter</a></p>
 </footer>
 
+<!-- Bootstrap JS -->
 <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js"></script>
 </body>
 </html>
