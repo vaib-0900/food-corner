bbd044522881471b5b336ad06643ae373e5fad4a
omrajyadav <omy696498@gmail.com>
2025-06-03 12:09:24 +0530

    same change

--- promotion.php
+++ promotion.php
@@ -14,12 +14,6 @@ include "header.php";
   <!-- Animate.css -->
   <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/animate.css/4.1.1/animate.min.css">
   <style>
-    :root {
-      --primary: #ff6b6b;
-      --secondary: #ffa502;
-      --dark: #2f3542;
-      --light: #f1f2f6;
-    }
     
     body {
       font-family: 'Poppins', sans-serif;
